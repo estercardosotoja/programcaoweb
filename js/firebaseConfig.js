300b9c1be3e7ba9e57ef96ef7d30f4dc862041c4
Ester Cardoso Toja <87045785+estercardosotoja@users.noreply.github.com>
2025-09-25 19:13:10 -0300

    Update firebaseConfig.js

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -5,14 +5,7 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth
 
 // Substitua pelos dados do seu projeto Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyAkiUWZM4FXP5-XxRHSDH8ALHK_g8wSMKw",
-  authDomain: "project-crud-450b5.firebaseapp.com",
-  databaseURL: "https://project-crud-450b5-default-rtdb.firebaseio.com",
-  projectId: "project-crud-450b5",
-  storageBucket: "project-crud-450b5.firebasestorage.app",
-  messagingSenderId: "408778519635",
-  appId: "1:408778519635:web:62c89d69f98d41524202a6",
-  measurementId: "G-9RHVF4EH7V"
+COLOQUE AQUI SUAS CREDENCIAIS
 };
 
 // Initialize Firebase
